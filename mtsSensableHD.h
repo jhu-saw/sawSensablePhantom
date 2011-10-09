108dde324b8c6fea6ec9edb8b958903939663018
Anton Deguet <anton.deguet@jhu.edu>
2011-10-09 01:53:36 +0000

    /trunk: major re-organization: introduction of cisst and saw top directories, re-organization of CMake configuration.
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk/saw/components/sawSensablePhantom@3034 0959eb1a-6956-4f60-947b-6092a4e22c97

---
+++ mtsSensableHD.h
@@ -0,0 +1,144 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-    */
+/* ex: set filetype=cpp softtabstop=4 shiftwidth=4 tabstop=4 cindent expandtab: */
+
+/*
+  $Id$
+
+  Author(s): Anton Deguet
+  Created on: 2008-04-04
+
+  (C) Copyright 2008-2011 Johns Hopkins University (JHU), All Rights
+  Reserved.
+
+--- begin cisst license - do not edit ---
+
+This software is provided "as is" under an open source license, with
+no warranty.  The complete license can be found in license.txt and
+http://www.cisst.org/cisst/license.txt.
+
+--- end cisst license ---
+*/
+
+#ifndef _mtsSensableHD_h
+#define _mtsSensableHD_h
+
+#include <cisstMultiTask/mtsTaskFromCallback.h>
+#include <cisstParameterTypes.h>
+
+// Always include last
+#include <cisstDevices/devExport.h>
+
+// forward declaration for private data
+struct mtsSensableHDDriverData;
+struct mtsSensableHDHandle;
+
+class CISST_EXPORT mtsSensableHD: public mtsTaskFromCallbackAdapter {
+    CMN_DECLARE_SERVICES(CMN_NO_DYNAMIC_CREATION, CMN_LOG_ALLOW_DEFAULT);
+
+public:
+    enum {NB_JOINTS = 6};
+    int DeviceCount;
+
+protected:
+    // internal data using Sensable data types
+    struct DeviceData {
+        bool DeviceEnabled;
+        bool ForceOutputEnabled;
+        
+        // local copy of the buttons state as defined by Sensable
+        mtsInt Buttons;
+        
+        // local copy of the position and velocities
+        prmPositionCartesianGet PositionCartesian;
+        prmVelocityCartesianGet VelocityCartesian;
+        prmPositionJointGet PositionJoint;
+        vctDynamicVectorRef<double> GimbalJointsRef;
+        
+        // mtsFunction called to broadcast the event
+        mtsFunctionWrite Button1Event, Button2Event;
+
+        prmForceCartesianSet ForceCartesian;
+
+        // local buffer used to store the position as provided
+        // by Sensable
+        typedef vctFixedSizeMatrix<double, 4, 4, VCT_COL_MAJOR> Frame4x4Type;
+        Frame4x4Type Frame4x4;
+        vctFixedSizeConstVectorRef<double, 3, Frame4x4Type::ROWSTRIDE> Frame4x4TranslationRef;
+        vctFixedSizeConstMatrixRef<double, 3, 3,
+                                   Frame4x4Type::ROWSTRIDE, Frame4x4Type::COLSTRIDE> Frame4x4RotationRef;
+        
+        // added to provide tip position to the frame manager
+        mtsFunctionRead PositionFunctionForTransformationManager;
+
+        bool Clutch;
+
+        std::string Name;
+
+        int DeviceNumber;
+    };
+
+    vctDynamicVector<DeviceData *> DevicesVector;
+    vctDynamicVector<mtsSensableHDHandle *> DevicesHandleVector;
+    mtsSensableHDDriverData * Driver;
+    void SetInterfaces(const std::string & firstDeviceName,
+                       const std::string & secondDeviceName,
+                       bool firstDeviceForcesEnabled,
+                       bool secondDeviceForcesEnabled);
+    void SetupInterfaces(void);
+
+private:
+    /*! Default constructor, will use the default device connected and
+      create and interface named "Default Arm" */
+    mtsSensableHD(const std::string & taskName);
+
+public:
+    /*! Constructor for a single arm with a user specified name.  The
+      name must match the device name as defined by Sensable
+      drivers. Force output initially disabled*/
+    mtsSensableHD(const std::string & taskName,
+                  const std::string & firstDeviceName);
+
+    /*! Constructor for a single arm with a user specified name.  The
+      name must match the device name as defined by Sensable
+      drivers. Overloaded to enable/disable force output.*/
+    mtsSensableHD(const std::string & taskName,
+                  const std::string & firstDeviceName,
+                  bool firstDeviceForcesEnabled);
+
+    /*! Constructor for two arms with a user specified names.  The
+      names must match the device names as defined by Sensable
+      drivers. Force output initially disabled */
+    //@{
+    mtsSensableHD(const std::string & taskName,
+                  const std::string & firstDeviceName,
+                  const std::string & secondDeviceName);
+
+    mtsSensableHD(const char * taskName,
+                  const char * firstDeviceName,
+                  const char * secondDeviceName);
+    //@}
+
+    /*! Constructor for two arms with a user specified names.  The
+      names must match the device names as defined by Sensable
+      drivers. Overloaded to enable/disable force output. */
+    mtsSensableHD(const std::string & taskName,
+                  const std::string & firstDeviceName,
+                  const std::string & secondDeviceName,
+                  bool firstDeviceForcesEnabled,
+                  bool secondDeviceForcesEnabled);
+
+    ~mtsSensableHD();
+    void Configure(const std::string & CMN_UNUSED(filename) = "") {};
+	void Create(void *data = 0);
+    void Run();
+	void Start(void);
+	void Kill(void);
+    void Cleanup(void) {};
+    virtual void UserControl(void) {};
+};
+
+
+CMN_DECLARE_SERVICES_INSTANTIATION(mtsSensableHD);
+
+
+#endif // _mtsSensableHD_h
