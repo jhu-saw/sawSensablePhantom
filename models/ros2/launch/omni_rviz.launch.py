1125ff2b731b568b581a3baf9dfe8b7671508426
Anton Deguet <anton.deguet@jhu.edu>
2026-04-06 19:09:06 -0400

    Added fake_hardware to launch file

--- models/ros2/launch/omni_rviz.launch.py
+++ models/ros2/launch/omni_rviz.launch.py
@@ -2,7 +2,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.substitutions import LaunchConfiguration, Command
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, Command, PythonExpression
 from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterValue
 
@@ -10,6 +11,7 @@ def generate_launch_description():
 
     rate = LaunchConfiguration('rate', default=50.0)  # Hz, default is 10 so we're increasing that a bit.  Funny enough joint and robot state publishers don't have the same name for that parameter :-(
     sensable_config = LaunchConfiguration('sensable_config')
+    fake_hardware = LaunchConfiguration('fake_hardware')
 
     # urdf config
     urdf_file_name = 'omni.urdf'
@@ -26,16 +28,13 @@ def generate_launch_description():
         rviz_file_name)
 
     # sensable config
-    sensable_arguments = []
-    if sensable_config != '':
-        sensable_arguments = ['-j', sensable_config]
-
     sensable_phantom_node = Node(
         package='sensable_phantom',
         executable='sensable_phantom',
         name='sensable_phantom',
         output='screen',
-        arguments=sensable_arguments
+        arguments=['-j', sensable_config],
+        condition=UnlessCondition(fake_hardware)
     )
 
     joint_state_publisher_node = Node(
@@ -45,7 +44,18 @@ def generate_launch_description():
         output='screen',
         parameters=[{'source_list': ['arm/measured_js'],
                      'rate': rate
-        }]
+        }],
+        condition=UnlessCondition(fake_hardware)
+    )
+
+    # Let's add a conditional for fake hardware that also uses joint_state_publisher
+    # but without source_list.  Wait, the user wants the GUI version!
+    fake_joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen',
+        condition=IfCondition(fake_hardware)
     )
 
     robot_state_publisher_node = Node(
@@ -73,8 +83,14 @@ def generate_launch_description():
             'sensable_config',
             default_value='',
             description='JSON configuration file for sawSensablePhantom'),
+        DeclareLaunchArgument(
+            'fake_hardware',
+            default_value='false',
+            choices=['true', 'false'],
+            description='If true, don\'t start the nodes for sensable_phantom and joint_state_publisher'),
         sensable_phantom_node,
         joint_state_publisher_node,
+        fake_joint_state_publisher_node,
         robot_state_publisher_node,
         rviz_node
     ])
